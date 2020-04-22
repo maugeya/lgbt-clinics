857ec12f9fada2f01f727145163ae6c6c622baf6
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-22 21:01:20 +0100

    Creating clinic model

--- backend/clinic/models.py
+++ backend/clinic/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Clinic(models.Model):
+    name = models.CharField(max_length=250)
+    clinic_code = models.CharField(max_length=50)
+    address_1 = models.CharField(max_length=100)
+    address_2 = models.CharField(max_length=100, null=True)
+    town = models.CharField(max_length=100, null=True)
+    county = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=100)
+    url = models.URLField(max_length=250)
+
