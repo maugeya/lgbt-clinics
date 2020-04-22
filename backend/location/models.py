707b28f1018413ad96c128eb6a01aaf0dd63aa14
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-22 21:25:13 +0100

    Model for location coordinates

--- backend/location/models.py
+++ backend/location/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=25, decimal_places=20)
+    longitude = models.DecimalField(max_digits=25, decimal_places=20)
+
+    def __str__(self):
+        return '{},{}'.format(
+            round(self.latitude, 10),
+            round(self.longitude, 10)
+        )
+
+    def __eq__(self, other):
+        return str(self) == str(other)
