04372fade7bb6d77ae39d3cfe40b2b90562bae07
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-22 20:32:44 +0100

    Initialising frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD
+    </div>
+  );
+}
+
+export default App;
