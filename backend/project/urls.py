c5621d8799013be92f1a5ab35b5509692148e3a7
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-22 20:50:19 +0100

    Using url instead of path

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
