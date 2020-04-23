5ad4ec76c7a1289ca9e38af27c728fb06fc3fe0a
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-23 14:55:57 +0100

    Adding new CustomUser model as default user model and adding new app to settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -21,9 +21,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "users",
     "location",
     "clinic",
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
