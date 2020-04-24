c2ece913a8b51ea9b76a4ee2a781c379b81be9cf
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-24 09:59:57 +0100

    Adding URL for graphql

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
