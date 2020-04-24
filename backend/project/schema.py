6d79f8062d75d6c759d85ef39d1a83d4ae240aef
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-24 14:44:06 +0100

    Adding location schema to root schema

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -2,12 +2,13 @@ import graphene
 from graphql_auth.schema import UserQuery, MeQuery
 from users.schema import AuthMutation
 import clinic.schema
+import location.schema
 
-class Query(clinic.schema.Query, UserQuery, MeQuery, graphene.ObjectType):
+class Query(clinic.schema.Query, location.schema.Query, UserQuery, MeQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(AuthMutation, graphene.ObjectType):
+class Mutation(clinic.schema.Mutation, AuthMutation, graphene.ObjectType):
    pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
