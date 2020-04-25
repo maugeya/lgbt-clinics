453fa3cc8875c5a43921c107591d02580e39c671
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-25 22:26:46 +0100

    Setting ApolloProvider for graphQL calls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import Auth from './components/Auth';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'http://localhost:8000/graphql/'
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Auth />
+    </ApolloProvider>
+    , document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
