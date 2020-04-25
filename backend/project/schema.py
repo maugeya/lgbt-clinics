17cfd46d8709760b3e622240200da42e0e2ecae8
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-25 16:35:52 +0100

    Updating imports

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphql_auth.schema import UserQuery, MeQuery
-from users.schema import AuthMutation
+import users.schema
 import clinic.schema
 import location.schema
 
@@ -8,7 +8,7 @@ class Query(clinic.schema.Query, location.schema.Query, UserQuery, MeQuery, grap
     pass
 
 
-class Mutation(clinic.schema.Mutation, AuthMutation, graphene.ObjectType):
+class Mutation(clinic.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
    pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
