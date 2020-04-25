342f3f427494e8522072eaa62b973989d9d3b972
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-25 16:36:10 +0100

    Updating schema for creating user

--- backend/users/schema.py
+++ backend/users/schema.py
@@ -1,28 +1,46 @@
+from django.contrib.auth import get_user_model
+from graphql import GraphQLError
 import graphene
 from graphene_django import DjangoObjectType
-from graphql_auth import mutations
-from users.models import CustomUser
 
 
 class UserType(DjangoObjectType):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
 
 
-class AuthMutation(graphene.ObjectType):
-    register = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
-    resend_activation_email = mutations.ResendActivationEmail.Field()
-    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
-    password_reset = mutations.PasswordReset.Field()
-    password_change = mutations.PasswordChange.Field()
-    archive_account = mutations.ArchiveAccount.Field()
-    delete_account = mutations.DeleteAccount.Field()
-    update_account = mutations.UpdateAccount.Field()
-    send_secondary_email_activation =  mutations.SendSecondaryEmailActivation.Field()
-    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
-    swap_emails = mutations.SwapEmails.Field()
-    token_auth = mutations.ObtainJSONWebToken.Field()
-    verify_token = mutations.VerifyToken.Field()
-    refresh_token = mutations.RefreshToken.Field()
-    revoke_token = mutations.RevokeToken.Field()
+class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
+    me = graphene.Field(UserType)
+
+    def resolve_user(self, info, id):
+        return get_user_model().objects.get(id=id)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('Not logged in!')
+
+        return user
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
