8aaefe330ab20bd5358d60087e4068dac7a947a2
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-28 14:58:33 +0100

    Adding Router to app component. Wrapping with a query to access currentuser information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
-import React from "react"
-import withRoot from "./withRoot"
-import { Query } from "react-apollo"
-import { gql } from "apollo-boost"
+import React from "react";
+import withRoot from "./withRoot";
+import { Query } from "react-apollo";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ME_QUERY, GET_CLINICS_QUERY } from "./library/graphQL";
+import Home from "./pages/Home";
+import ClinicsRoute from "./pages/ClinicsRoute";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header";
+import Loading from "./components/Shared/Loading";
+import Error from "./components/Shared/Error";
 
 const Root = () => (
-    <Query query={GET_CLINICS_QUERY}>
+    <Query query={ME_QUERY}>
         {({ data, loading, error }) => {
-            if (loading) return <div>Loading</div>
-            if (error) return <div>Error</div>
+            if (loading) return <Loading />
+            if (error) return <Error error={error} />
+            const currentUser = data.me
 
-        return <div>{JSON.stringify(data)}</div>
+        return (
+            <Router>
+                <>
+                    <Header currentUser={currentUser}/>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/clinics" component={ClinicsRoute} />
+                        <Route path="/profile/:id" component={Profile} />
+                    </Switch>
+                </>
+            </Router>
+        )
         }}
     </Query>
 )
-
-const GET_CLINICS_QUERY = gql`
-    {
-        clinics {
-            id
-            name
-            clinicCode
-            url
-        }
-    }
-`
 
 export default withRoot(Root)
