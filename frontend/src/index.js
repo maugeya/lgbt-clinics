e883391db0979ed98ad8c8ff41c7b507de603fee
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-04-28 11:12:45 +0100

    Implementing knowledge of IS_LOGGED_IN query to render correct page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import Auth from './components/Auth';
+import { IS_LOGGED_IN } from "./library/graphQL"
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql/'
-})
+    uri: 'http://localhost:8000/graphql/',
+    clientState: {
+        defaults: {
+            isLoggedIn: !!localStorage.getItem('authToken')
+        }
+    }
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Auth />
+        <Query query={IS_LOGGED_IN}>
+            {({ data }) => data.isLoggedIn ? <App /> : <Auth/>}
+        </Query>
     </ApolloProvider>
     , document.getElementById('root')
-    );
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
