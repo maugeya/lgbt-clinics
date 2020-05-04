e51d530c3567afcde76481859890de4ae0faf269
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-04 16:11:57 +0100

    Updaing index schema to include correct use of user schema

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,11 +1,10 @@
 import graphene
 import graphql_jwt
-from graphql_auth.schema import UserQuery, MeQuery
 import users.schema
 import clinic.schema
 import location.schema
 
-class Query(clinic.schema.Query, location.schema.Query, UserQuery, MeQuery, graphene.ObjectType):
+class Query(users.schema.Query, clinic.schema.Query, location.schema.Query, graphene.ObjectType):
     pass
 
 
